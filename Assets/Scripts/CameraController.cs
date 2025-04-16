4afef2898c50eb32d75ab6cbbd762e73099e5daa
MrRuho <paajanen.ville@gmail.com>
2025-04-16 11:22:29 +0300

    Add camera control. Player can control camera.( Move, rotate and zoom.)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
+
+    private CinemachineFollow cinemachineFollow;
+    private Vector3 targetFollowOffset;
+
+    private float moveSpeed = 10f;
+    private float rotationSpeed = 100f;
+    private float zoomSpeed = 5f;
+    
+    private void Start()
+    {
+        cinemachineFollow = cinemachineCamera.GetComponent<CinemachineFollow>();
+        targetFollowOffset = cinemachineFollow.FollowOffset;
+    }
+
+    private void Update()
+    {
+        HandleMovement(moveSpeed);
+        HandleRotation(rotationSpeed);
+        HandleZoom(zoomSpeed);
+    }
+
+    private void HandleMovement(float moveSpeed)
+    {
+        Vector3 inputMoveDirection = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDirection.z = +1f;
+        }  
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDirection.z = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDirection.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDirection.x = +1f;
+        }
+
+        Vector3 moveVector = transform.forward * inputMoveDirection.z + transform.right * inputMoveDirection.x;
+        transform.position += moveSpeed * Time.deltaTime * moveVector;
+    }
+
+    private void HandleRotation(float rotationSpeed)
+    {
+        Vector3 rotationVector = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y = +1f;
+        }
+
+        transform.eulerAngles += rotationSpeed * Time.deltaTime * rotationVector;
+    }
+
+    private void HandleZoom(float zoomSpeed)
+    {
+        float zoomAmount = 1f;
+        if(Input.mouseScrollDelta.y > 0) 
+        {
+            targetFollowOffset.y -= zoomAmount;
+        }
+        if(Input.mouseScrollDelta.y < 0) 
+        {
+            targetFollowOffset.y += zoomAmount;
+        }
+
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        cinemachineFollow.FollowOffset = Vector3.Lerp(cinemachineFollow.FollowOffset, targetFollowOffset, Time.deltaTime * zoomSpeed);
+    }
+
+}
