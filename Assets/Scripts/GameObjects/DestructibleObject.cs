f863dc6d2c8766722f16d4c830ec99b0990c425f
MrRuho <paajanen.ville@gmail.com>
2025-09-25 20:24:46 +0300

    GamedevTV 7.6 Destructible Crate Parts

--- Assets/Scripts/GameObjects/DestructibleObject.cs
+++ Assets/Scripts/GameObjects/DestructibleObject.cs
@@ -6,9 +6,10 @@ public class DestructibleObject : MonoBehaviour
     public static event EventHandler OnAnyDestroyed;
 
     private GridPosition gridPosition;
+    [SerializeField] private Transform objectDestroyPrefab;
     [SerializeField] private int health = 3;
 
- 
+
 
     // To prevent multiple destruction events
     private bool isDestroyed;
@@ -20,7 +21,7 @@ public class DestructibleObject : MonoBehaviour
 
     private void Start()
     {
-      gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
     }
 
     public GridPosition GetGridPosition()
@@ -40,9 +41,24 @@ public class DestructibleObject : MonoBehaviour
             if (!isDestroyed)
             {
                 isDestroyed = true;
+                Transform createDestroyTransform = Instantiate(objectDestroyPrefab, transform.position, Quaternion.identity);
+                ApplyPushForceToChildren(createDestroyTransform, 300f, transform.position, 10f);
                 Destroy(gameObject);
                 OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
             }
+        }
+    }
+    
+    private void ApplyPushForceToChildren(Transform root, float pushForce, Vector3 pushPosition, float PushRange)
+    {
+        foreach (Transform child in root)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(pushForce, pushPosition, PushRange);
+            }
+
+            ApplyPushForceToChildren(child, pushForce, pushPosition, PushRange);
         }
     }
 }
